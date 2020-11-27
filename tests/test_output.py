1a94872d5033240acaaf85211fdddabbad97ef6f
Andrei Cuceu <andrei.cuceu.14@ucl.ac.uk>
2020-11-27 17:11:07 +0000

    Added test output. Doesn't work yet

---
+++ tests/test_output.py
@@ -0,0 +1,25 @@
+# import pytest
+# import numpy as np
+# from astropy.io import fits
+
+# from vega import VegaInterface
+# from vega.utils import find_file
+
+
+# def test_output():
+#     # hdul = fits.open(find_file('data/picca_bench_data.fits'))
+#     # names = ['test_' + str(i) for i in range(8)]
+
+#     vega_auto = VegaInterface(
+#         'examples/picca_benchmarks/configs/vega/main.ini')
+
+#     xi_vega_auto = vega_auto.compute_model(run_init=True)
+
+#     for name in names:
+#         xi_picca_auto = np.array(hdul[1].data['auto_' + name])
+#         xi_picca_cross = np.array(hdul[2].data['cross_' + name])
+
+#         assert np.allclose(xi_vega_auto[name], xi_picca_auto)
+#         assert np.allclose(xi_vega_cross[name], xi_picca_cross)
+
+#     hdul.close()
