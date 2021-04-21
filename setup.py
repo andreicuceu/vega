da8793b8e7b25cf5f8b00f474450279b40e02214
Andrei Cuceu <andrei.cuceu.14@ucl.ac.uk>
2021-04-21 17:51:11 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3',E
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
     ],
