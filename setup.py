0f381020fcc3364e0dc7f168a99a10bf28c9e76a
Andrei Cuceu <andrei.cuceu.14@ucl.ac.uk>
2021-04-28 11:00:15 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['numpy', 'scipy', 'astropy', 'numba', 'iminuit', 'h5py', 'mcfit,
+requirements = ['numpy', 'scipy', 'astropy', 'numba', 'iminuit', 'h5py', 'mcfit',
                 'setuptools']
 
 setup_requirements = ['pytest-runner', ]
