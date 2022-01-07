66dc1ddce69d33deb8c9d811931fe368b3434ec5
Andrei Cuceu <andrei.cuceu.14@ucl.ac.uk>
2022-01-07 09:48:03 -0500

    Add blinding strategy corr_yshift

--- vega/data.py
+++ vega/data.py
@@ -142,9 +142,16 @@ class Data:
         if 'BLINDING' in hdul[1].header:
             blinding = hdul[1].header['BLINDING']
 
-        if blinding == 'minimal':
+        if blinding == 'corr_yshift':
             print('Warning! Running on blinded data {}'.format(data_path))
-            print('Scale parameters must be fixed to 1.')
+            print('Strategy: corr_yshift. BAO can be sampled')
+            self._blind = False
+            self._data_vec = hdul[1].data['DA_BLIND']
+            if 'DM' in hdul[1].columns.names:
+                self._distortion_mat = csr_matrix(hdul[1].data['DM_BLIND'])
+        elif blinding == 'minimal':
+            print('Warning! Running on blinded data {}'.format(data_path))
+            print('Strategy: minimal. Scale parameters must be fixed to 1.')
             self._blind = True
             self._data_vec = hdul[1].data['DA_BLIND']
             if 'DM' in hdul[1].columns.names:
