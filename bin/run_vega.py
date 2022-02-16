2bd5a05784564fcfda4ebebaa38f480fd4b61120
Andrei Cuceu <andrei.cuceu.14@ucl.ac.uk>
2022-02-16 11:13:28 +0000

    Remove control requirement from run_vega.py

--- bin/run_vega.py
+++ bin/run_vega.py
@@ -9,21 +9,22 @@ def run_vega(config_path):
     vega = VegaInterface(config_path)
 
     # Check if we need to run over a Monte Carlo mock
-    run_montecarlo = vega.main_config['control'].getboolean('run_montecarlo', False)
-    if run_montecarlo and vega.mc_config is not None:
-        # Get the MC seed and forecast flag
-        seed = vega.main_config['control'].getfloat('mc_seed', 0)
-        forecast = vega.main_config['control'].getboolean('forecast', False)
+    if 'control' in vega.main_config:
+        run_montecarlo = vega.main_config['control'].getboolean('run_montecarlo', False)
+        if run_montecarlo and vega.mc_config is not None:
+            # Get the MC seed and forecast flag
+            seed = vega.main_config['control'].getfloat('mc_seed', 0)
+            forecast = vega.main_config['control'].getboolean('forecast', False)
 
-        # Create the mocks
-        vega.monte_carlo_sim(vega.mc_config['params'], seed=seed, forecast=forecast)
+            # Create the mocks
+            vega.monte_carlo_sim(vega.mc_config['params'], seed=seed, forecast=forecast)
 
-        # Set to sample the MC params
-        sampling_params = vega.mc_config['sample']
-        vega.minimizer = Minimizer(vega.chi2, sampling_params)
-    elif run_montecarlo:
-        raise ValueError('You asked to run over a Monte Carlo simulation,'
-                         ' but no "[monte carlo]" section provided.')
+            # Set to sample the MC params
+            sampling_params = vega.mc_config['sample']
+            vega.minimizer = Minimizer(vega.chi2, sampling_params)
+        elif run_montecarlo:
+            raise ValueError('You asked to run over a Monte Carlo simulation,'
+                             ' but no "[monte carlo]" section provided.')
 
     # run compute_model once to initialize all the caches
     _ = vega.compute_model(run_init=False)
