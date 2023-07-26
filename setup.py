0b1db67ad1ddeb392c4d1220d54ccccc88ec28ea
andreicuceu <andreicuceu@gmail.com>
2023-07-26 15:40:32 +0100

    Bump version: 0.4.2 → 0.4.3

--- setup.py
+++ setup.py
@@ -52,6 +52,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/andreicuceu/Vega',
-    version='0.4.2',
+    version='0.4.3',
     zip_safe=False,
 )
