dca71e2c454edfd87467b5e364882de295f2eaa7
andreicuceu <andreicuceu@gmail.com>
2023-12-02 17:49:39 -0500

    Bump version: 0.5.2 → 0.6.0

--- setup.py
+++ setup.py
@@ -52,6 +52,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/andreicuceu/Vega',
-    version='0.5.2',
+    version='0.6.0',
     zip_safe=False,
 )
