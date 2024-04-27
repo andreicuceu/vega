59718957eb674549a7e206db11f89ccbbcd56712
Andrei Cuceu <andreicuceu@gmail.com>
2024-04-27 19:02:22 -0400

    Add AP blinding for DR1

--- vega/scale_parameters.py
+++ vega/scale_parameters.py
@@ -1,4 +1,5 @@
 import numpy as np
+from pathlib import Path
 
 
 class ScaleParameters:
@@ -9,7 +10,7 @@ class ScaleParameters:
     Lya AP: phi = at/ap, alpha = sqrt(ap * at)
     See 2.1 of https://arxiv.org/pdf/2103.14075.pdf for more details.
     """
-    def __init__(self, config):
+    def __init__(self, config, blind_pars=None):
         """Initialize scale parameters from the cosmo-fit type config
 
         Parameters
@@ -22,19 +23,28 @@ class ScaleParameters:
         self.smooth_scaling = config.getboolean('smooth-scaling', False)
         self.metal_scaling = config.getboolean('metal-scaling', False)
 
-        self.blind_phi_smooth = config.getboolean('blind-phi_smooth', False)
-        self._rnsps = None
-        if self.blind_phi_smooth:
-            seed = config.getint('seed')
-            size = config.getfloat('size')
-            rng = np.random.default_rng(seed)
-            self._rnsps = np.sqrt(np.log(np.pi - rng.uniform(-size/2, size/2)))
+        self._rnsps = 0.
+        self.blind_phi_smooth = False
+        if blind_pars is not None and len(blind_pars) > 0:
+            if 'phi_smooth' not in blind_pars:
+                raise ValueError('Only phi_smooth blinding implemented.')
+
+            blind_dir = '/global/cfs/projectdirs/desicollab/science/lya/vega/full-shape-blinding/'
+            blinding_file = Path(blind_dir) / 'dr1_ap_blinding_27_04_2024.npz'
+            if not blinding_file.exists():
+                raise ValueError(f'Blinding file not found: {blinding_file}.'
+                                 'Full-shape analyses must be run at NERSC.')
+
+            with np.load(blinding_file) as file:
+                self._rnsps = float(file['phi_smooth'])
+            self.blind_phi_smooth = True
+
         elif self.full_shape or self.smooth_scaling:
             print('Warning! Running full-shape without blinding.')
 
-        if self.full_shape or self.smooth_scaling:
-            print('WARNING!!!: Using full-shape fit or scaling of the smooth cf component. '
-                  'Sailor you are reaching unexplored territories, precede at your own risk.')
+        # if self.full_shape or self.smooth_scaling:
+            # print('WARNING!!!: Using full-shape fit or scaling of the smooth cf component. '
+                #   'Sailor you are reaching unexplored territories, precede at your own risk.')
 
         self.parametrisation = config.get('cosmo fit func', 'ap_at')
         if self.parametrisation not in ['ap_at', 'aiso_epsilon', 'phi_alpha']:
