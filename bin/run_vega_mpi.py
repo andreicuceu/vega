cf0d4b980df6e6c49f4a06ceb968dbfcda419e2e
Andrei Cuceu <andreicuceu@gmail.com>
2024-06-10 16:55:29 -0400

    Modify pocomc state folder

--- bin/run_vega_mpi.py
+++ bin/run_vega_mpi.py
@@ -16,6 +16,7 @@ if __name__ == '__main__':
 
     mpi_comm = MPI.COMM_WORLD
     cpu_rank = mpi_comm.Get_rank()
+    num_cpus = mpi_comm.Get_size()
 
     def print_func(message):
         if cpu_rank == 0:
@@ -74,6 +75,8 @@ if __name__ == '__main__':
 
         print_func('Running PocoMC')
         sampler = PocoMC(vega.main_config['PocoMC'], sampling_params, vega.log_lik)
+        if cpu_rank == 0:
+            sampler.pocomc_output.mkdir()
 
         def log_lik(theta):
             params = {name: val for name, val in zip(sampler.names, theta)}
@@ -83,18 +86,21 @@ if __name__ == '__main__':
             mpi_comm.barrier()
             with MPIPool(mpi_comm) as pool:
                 pocomc_sampler = pocomc.Sampler(
-                    sampler.prior, log_lik,
-                    pool=pool, output_dir=sampler.path,
+                    sampler.prior, log_lik, pool=pool, output_dir=sampler.pocomc_output,
                     dynamic=sampler.dynamic, precondition=sampler.precondition,
                     n_effective=sampler.n_effective, n_active=sampler.n_active,
                 )
                 pocomc_sampler.run(
                     sampler.n_total, sampler.n_evidence, save_every=sampler.save_every)
         else:
+            if num_cpus > 1:
+                raise ValueError(
+                    'You asked to run PocoMC without MPI, '
+                    'but you are using more than one MPI thread.'
+                )
             with Pool(sampler.num_cpu) as pool:
                 pocomc_sampler = pocomc.Sampler(
-                    sampler.prior, log_lik,
-                    pool=pool, output_dir=sampler.path,
+                    sampler.prior, log_lik, pool=pool, output_dir=sampler.pocomc_output,
                     dynamic=sampler.dynamic, precondition=sampler.precondition,
                     n_effective=sampler.n_effective, n_active=sampler.n_active,
                 )
