888942ece80af0ed2418f4daaf3ec2052c5d295a
wynneturner04@gmail.com <wynneturner04@gmail.com>
2024-11-22 14:27:46 -0800

    commented likelihood code

--- cobaya_interface/likelihood.py
+++ cobaya_interface/likelihood.py
@@ -12,19 +12,22 @@ from scipy.interpolate import interp1d, InterpolatedUnivariateSpline
 from typing import Optional
 
 
-class Likelihood(Likelihood):
+class Likelihood(Likelihood): # class that inherits from cobaya.likelihood
 
     def initialize(self, **params_values):
-
-        #print(params_values)
-        self.vega = VegaInterface('simple_example/complex_main.ini')
+        '''
+        Set up initial parameters
+        '''
+        self.vega = VegaInterface('simple_example/complex_main.ini') # Creates an instance of VegaInterface with a configuration file containing cosmological or model parameters
         self.effective_redshift = 2.3
-        self.k_grid = np.logspace(-4,2,700)
+        self.k_grid = np.logspace(-4,2,700) # grid of scales for power spectrum
         self.vega.fiducial['z_fiducial'] = self.effective_redshift   # fix z_eff z_fiducial
 
 
     def get_requirements(self):
-        
+        '''
+        Specifies what cosmological parameters are required by cobaya
+        '''
         return {'bias_LYA': None, 'beta_LYA': None,'D_M_fid': None, 'D_H_fid': None, 'or_photon':None, 'or_neutrino':None, \
                 'H0': None, 'ombh2': None , 'omch2': None, 'omnuh2': None, 'omk': None,'As': None, 'ns': None, \
                 'Pk_grid': {'z': [self.effective_redshift], 'k_max':100, 'nonlinear':[False]},
@@ -32,28 +35,30 @@ class Likelihood(Likelihood):
                 }
 
     def logp(self, **params_values):
-
-        scale_factor = 1/(1+self.effective_redshift)
-        h = params_values['H0']/100
-        D_M = (self.provider.get_angular_diameter_distance(self.effective_redshift)/scale_factor)*h
-        D_H = ((scipy.constants.c/1000)/self.provider.get_Hubble(self.effective_redshift))*h
-        params_values['ap_full'] = D_H[0]/params_values['D_H_fid']
-        params_values['at_full'] = D_M[0] / params_values['D_M_fid']
+        '''
+        Method to calculate the log-likelihood based on the parameters provided
+        '''
+        scale_factor = 1/(1+self.effective_redshift) # scale factor at effective redshift
+        h = params_values['H0']/100 # defines the reduced Hubble constant from provided H0
+        D_M = (self.provider.get_angular_diameter_distance(self.effective_redshift)/scale_factor)*h # computes angular diameter distance to effective redshift 
+        D_H = ((scipy.constants.c/1000)/self.provider.get_Hubble(self.effective_redshift))*h # computes Hubble distance
+        params_values['ap_full'] = D_H[0]/params_values['D_H_fid'] # computes alpha_parallel 
+        params_values['at_full'] = D_M[0]/params_values['D_M_fid'] # computes alpha_transverse
         
-        k_Mpc, z, pk_Mpc = self.provider.get_Pk_grid(nonlinear = False)
+        k_Mpc, z, pk_Mpc = self.provider.get_Pk_grid(nonlinear = False) # retrieves power spectrum
         cs = interp1d(np.array(k_Mpc/h), np.array(pk_Mpc*(h**3)), kind='cubic')
-        k_hMpc = self.k_grid
-        pk_hMpc = cs(k_hMpc)
+        k_hMpc = self.k_grid # initial grid is in units of Mpc/h
+        pk_hMpc = cs(k_hMpc) # interpolates retrieved power spectrum onto this grid
         
-        if 'pk_full' not in self.vega.fiducial.keys():
+        if 'pk_full' not in self.vega.fiducial.keys(): # if vega fiducial pk_full is empty, store it here
             #print("self.vega.fiducial is empty")
             self.vega.fiducial['k'] = k_hMpc
             self.vega.fiducial['pk_full'] = pk_hMpc
         self.pk_full = pk_hMpc
             
-        omega_m =(params_values['ombh2'] + params_values['omch2'] + params_values['omnuh2'])/(h**2)
+        omega_m = (params_values['ombh2'] + params_values['omch2'] + params_values['omnuh2'])/(h**2) # computes Omega_m from baryons, cold dark matter, and non-relativistic neutrinos 
         self.vega.fiducial['Omega_m'] = omega_m
-        self.vega.fiducial['Omega_de'] = (1 - params_values['omk'] - params_values['or_photon'] - params_values['or_neutrino'] - omega_m) 
+        self.vega.fiducial['Omega_de'] = (1 - params_values['omk'] - params_values['or_photon'] - params_values['or_neutrino'] - omega_m) # computes Omega_de from Omega_m, flatness, photons, and relativistic neutrinos
         
-        chi2 = self.vega.chi2(params_values, direct_pk = self.pk_full)
-        return -chi2 / 2
+        chi2 = self.vega.chi2(params_values, direct_pk = self.pk_full) # uses vega to compute chi2 from power spectrum and parameter values 
+        return -chi2 / 2 # returns log likelihood
